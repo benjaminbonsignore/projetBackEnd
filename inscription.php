06025cb2f31559281acc92654dafee580cb29517
Benjamin Bonsignore <benjaminbonsignore@outlook.fr>
2019-01-07 14:43:07 +0100

    Premier envoi

---
+++ inscription.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta charset="utf-8">
+	    <link rel="stylesheet" type="text/css" href="styles/style.css">
+	    <title>Page d'inscription</title>
+	</head>
+
+	<body>
+
+
+	</body>
+</html>
